718c830e006d5e3d1d39061166170701e267c20f
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-03 15:06:12 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
+++ 240803/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -9,7 +9,7 @@ public class Main {
 
         if (n%2 == 0){
             return getSum(n-2) + n;
-        }
+        } //조건문을 기준으로 두 개의 반환식이 같은 형태이므로 조건문을 넣을 필요가 없음
         else{
             return getSum(n-2) + n;
         }
