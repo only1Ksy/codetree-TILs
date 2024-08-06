73fdad466d6af1b031424821b2ea97eb4fb85736
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-06 17:35:08 +0900

    Title: k번째로 신기한 문자열, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240806/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int k = input.nextInt();
+        //nextInt()와 nextLine()을 겹쳐 사용하면 문제 발생. 따라서 빈칸 사이에 두고 
+        //int와 String 입력 받아야 할 때는 string을 next로 읽고 그 뒤에 nextLine을 추가해 해결
+        String t = input.next();
+        input.nextLine();
+
+        String [] arr = new String[n];
+
+        for (int i = 0; i < n; i++){
+            arr[i] = input.nextLine();
+        }
+
+        Arrays.sort(arr);
+        int num = 0;
+
+        for (int i = 0; i < n; i++){
+            if (num == k){
+                System.out.println(arr[i-1]);
+                break;
+            }
+
+            if (arr[i].startsWith(t)){
+                num += 1;
+            }
+        }
+    }
+}
