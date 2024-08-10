dc1f7fd57ec3ed9f8874bbbaaf3c996d531bb062
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-10 15:00:06 +0900

    Title: Date to Date, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240810/Date to Date/date-to-date.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static int [] arr = new int [] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int m1 = input.nextInt();
+        int d1 = input.nextInt();
+        int m2 = input.nextInt();
+        int d2 = input.nextInt();
+
+        int days = 1;
+
+        for (int i = m2-1; i >= m1-1; i--){
+            if (i == m2-1)
+                days += d2;
+            else if (i == m1-1)
+                days += arr[i] - d1;
+            else
+                days += arr[i];
+        }
+
+        System.out.println(days);
+    }
+}
