77a59259db0cb448b0da5608a555c3e24ae6f6c2
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-12 21:04:02 +0900

    Title: 왔다 갔던 구역 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240812/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int [] arr = new int[200];
+        int n = input.nextInt();
+        input.nextLine();
+
+        int index = 100;
+
+        for (int i = 0; i < n; i++){
+            int x = input.nextInt();
+            String c = input.nextLine().trim(); 
+            //엔터 같이 입력되는 것 방지 위해 뒤에 .trim() 함수를 붙임
+
+            if (c.equals("R")){
+                for (int j = index; j < index + x; j++){
+                    arr[j] += 1;
+                }
+                index += x;
+            }
+            else{
+                for (int j = index; j > index - x; j--){
+                    arr[j] += 1;
+                }
+                index -= x;
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < 200; i++){
+            if (arr[i] >= 2)
+                result += 1;
+        }
+
+        System.out.println(result);
+    }
+}
