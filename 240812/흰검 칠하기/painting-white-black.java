68e9e164d71dec6a9da140c04194827c43009e28
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-12 22:05:57 +0900

    Title: 흰검 칠하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240812/흰검 칠하기/painting-white-black.java
+++ 240812/흰검 칠하기/painting-white-black.java
@@ -3,15 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        char[] arr = new char[2000];
-        int[] wArr = new int[2000];  // 흰색 덧칠 횟수
-        int[] bArr = new int[2000];  // 검은색 덧칠 횟수
+        char[] arr = new char[2000];  // 타일 배열
+        int[] wArr = new int[2000];   // 흰색 덧칠 횟수
+        int[] bArr = new int[2000];   // 검은색 덧칠 횟수
 
-        // 배열 초기화
-        Arrays.fill(arr, ' ');  // 공백으로 초기화
+        Arrays.fill(arr, ' ');  // 배열 초기화: 공백으로 설정
 
         int n = input.nextInt();
-        int index = 1000;
+        int index = 1000;  // 중앙 위치에서 시작
 
         for (int i = 0; i < n; i++) {
             int x = input.nextInt();
@@ -19,25 +18,31 @@ public class Main {
 
             if (s.equals("R")) {
                 for (int j = index; j < index + x; j++) {
-                    if (arr[j] != 'G') {  // 회색이 아닌 경우만 처리
-                        bArr[j]++;
-                        if (bArr[j] > 1 && wArr[j] > 1) {
-                            arr[j] = 'G';  // B와 W가 각각 2번 이상 칠해지면 G로 변환
-                        } else if (arr[j] == ' ' || arr[j] == 'W') {
-                            arr[j] = 'B';  // 초기 상태 또는 W 위에 B를 덧칠
-                        }
+                    if (arr[j] == ' ') {  // 초기 상태
+                        arr[j] = 'B';
+                    } else if (arr[j] == 'W') {  // 흰색 위에 검은색 덧칠
+                        arr[j] = 'G';
+                    } else if (arr[j] == 'G') {  // 회색 상태는 변경 없음
+                        continue;
+                    }
+                    bArr[j]++;
+                    if (bArr[j] > 1 && wArr[j] > 1) {
+                        arr[j] = 'G';
                     }
                 }
                 index += x;
             } else if (s.equals("L")) {
                 for (int j = index - 1; j >= index - x; j--) {
-                    if (arr[j] != 'G') {  // 회색이 아닌 경우만 처리
-                        wArr[j]++;
-                        if (wArr[j] > 1 && bArr[j] > 1) {
-                            arr[j] = 'G';  // B와 W가 각각 2번 이상 칠해지면 G로 변환
-                        } else if (arr[j] == ' ' || arr[j] == 'B') {
-                            arr[j] = 'W';  // 초기 상태 또는 B 위에 W를 덧칠
-                        }
+                    if (arr[j] == ' ') {  // 초기 상태
+                        arr[j] = 'W';
+                    } else if (arr[j] == 'B') {  // 검은색 위에 흰색 덧칠
+                        arr[j] = 'G';
+                    } else if (arr[j] == 'G') {  // 회색 상태는 변경 없음
+                        continue;
+                    }
+                    wArr[j]++;
+                    if (wArr[j] > 1 && bArr[j] > 1) {
+                        arr[j] = 'G';
                     }
                 }
                 index -= x;
