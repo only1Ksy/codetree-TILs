40b0ea68aaaea07d50cc42f512a25467a43043f0
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-12 21:55:42 +0900

    Title: 흰검 칠하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240812/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char [] arr = new char[2000];
+        int [] wArr = new int [2000];
+        int [] bArr = new int [2000];
+
+        int n = input.nextInt();
+        int index = 1000;
+
+        for (int i = 0; i < n; i++){
+            int x = input.nextInt();
+            String s = input.next();
+
+            if (s.equals("R")){
+                for (int j = index; j < index + x; j++){
+                    bArr[j]++;
+                    if (bArr[j] > 1 && wArr[j] > 1)
+                        arr[j] = 'G';
+                    else if (arr[j] == 'G')
+                        continue;
+                    else
+                        arr[j] = 'B';
+                }
+                index += x;
+            }
+            else{
+                for (int j = index - 1; j >= index - x; j--){
+                    wArr[j]++;
+                    if (bArr[j] > 1 && wArr[j] > 1)
+                        arr[j] = 'G';
+                    else if (arr[j] == 'G')
+                        continue;
+                    else
+                        arr[j] = 'W';
+                }
+                index -= x;
+            }
+        }
+
+        int wNum = 0;
+        int bNum = 0;
+        int gNum = 0;
+
+        for (int i = 0; i < 2000; i++){
+            if (arr[i] == 'W')
+                wNum++;
+            else if (arr[i] == 'B')
+                bNum++;
+            else if (arr[i] == 'G')
+                gNum++;
+        }
+
+        System.out.println(wNum + " " + bNum + " " + gNum);
+    }
+}
