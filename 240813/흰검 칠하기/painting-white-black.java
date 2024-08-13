998d36d057f0acb133af7315b42cfb1b95d67312
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-13 15:46:49 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240813/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char [] arr = new char[2000];
+        int [] wArr = new int [2000];
+        int [] bArr = new int [2000];
+
+        int n = input.nextInt();
+        int index = 1000;
+
+        for (int i = 0; i < n; i++){
+            int x = input.nextInt();
+            String s = input.next();
+
+            if (s.equals("R")){
+                for (int j = index; j < index + x; j++){
+                    if (arr[j] != 'G'){
+                        bArr[j]++;
+                        if (bArr[j] > 1 && wArr[j] > 1)
+                            arr[j] = 'G';
+                        else
+                            arr[j] = 'B';
+                    }
+                }
+                index += x - 1;
+            }
+            else{
+                for (int j = index; j > index - x; j--){
+                    if (arr[j] != 'G'){
+                        wArr[j]++;
+                        if (bArr[j] > 1 && wArr[j] > 1)
+                            arr[j] = 'G';
+                        else
+                            arr[j] = 'W';
+                    }
+                }
+                index -= x - 1; //전체를 빼는 계산이라는 것에 유의하여 부호 결정
+            }
+        }
+
+        int wNum = 0;
+        int bNum = 0;
+        int gNum = 0;
+
+        for (int i = 0; i < 2000; i++){
+            if (arr[i] == 'W')
+                wNum++;
+            else if (arr[i] == 'B')
+                bNum++;
+            else if (arr[i] == 'G')
+                gNum++;
+        }
+
+        System.out.println(wNum + " " + bNum + " " + gNum);
+    }
+}
