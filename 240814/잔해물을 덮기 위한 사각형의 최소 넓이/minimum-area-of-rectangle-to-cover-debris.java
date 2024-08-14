dabfb04db1e9b898a4e1f90ec543a0525de35f44
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-14 17:30:02 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 190ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240814/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int [][] arr = new int [2000][2000];
+
+        //A의 넓이 입력
+        int x1 = input.nextInt();
+        int y1 = input.nextInt();
+
+        int x2 = input.nextInt();
+        int y2 = input.nextInt();
+            
+        for (int j = x1 + 1000; j < x2 + 1000; j++){
+            for (int k = y1 + 1000; k < y2 + 1000; k++){
+                arr[j][k] += 1;
+            }
+        }
+        
+
+        //B의 넓이 제외
+        int x11 = input.nextInt();
+        int y11 = input.nextInt();
+
+        int x22 = input.nextInt();
+        int y22 = input.nextInt();
+            
+        for (int j = x11 + 1000; j < x22 + 1000; j++){
+            for (int k = y11 + 1000; k < y22 + 1000; k++){
+                arr[j][k] -= 1;
+            }
+        }
+        
+        //남은 구간 넓이의 합
+        int firstIndexX = 0;
+        int firstIndexY = 0;
+        int lastIndexX = 0;
+        int lastIndexY = 0;
+
+        for (int j = 0; j < 2000; j++){
+            for (int k = 0; k < 2000; k++){
+                if (arr[j][k] > 0){
+                    firstIndexX = j;
+                    firstIndexY = k;
+                    break;
+                }            
+            }
+            if (firstIndexX != 0) break;
+        }
+
+        for (int j = 1999; j >= 0; j--){
+            for (int k = 1999; k >= 0; k--){
+                if (arr[j][k] > 0){
+                    lastIndexX = j;
+                    lastIndexY = k;
+                    break;
+                }            
+            }
+            if (lastIndexX != 0) break;
+        }
+
+        System.out.println((lastIndexX - firstIndexX + 1) * (lastIndexY - firstIndexY + 1));
+        
+    }
+}
