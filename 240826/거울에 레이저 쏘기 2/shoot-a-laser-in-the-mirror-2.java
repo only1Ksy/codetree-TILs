6ef84090cd981be8686c229c6d46cd22462c9333
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-26 19:49:40 +0900

    Title: 거울에 레이저 쏘기 2, Time: 224ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240826/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240826/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -32,42 +32,26 @@ public class Main {
         int x = 0;
         int y = 0;
 
-        //초기 좌표 설정
-        int [][] ary = new int[n][n];
-
-        int num = 1; //오른쪽부터 시작
-
-        ary[0][0] = 1;
-
-        for (int i = 2; i < (n*4); i++){
-            if (k == 1 || k == n*4)
-                break;
-
-            int nx = x + dx[num];
-            int ny = y + dy[num];
-
-            if (!inRange(nx, ny, n))
-                num = (num - 1 + 4) % 4;
-            
-            x = x + dx[num];
-            y = y + dy[num];
-            ary[x][y] = i;
-
-            if (ary[x][y] == k)
-                break;
-        }
-        
-        //초기 방향 설정
         int dirNum = 0;
 
-        if (k > 0 && k <= n)
-            dirNum = 2;
-        else if (k > n && k <= n*2)
-            dirNum = 3;
-        else if (k > n*2 && k <= n*3)
-            dirNum = 0;
-        else
-            dirNum = 1;
+        // 시작 위치 k에 따라 초기 좌표(x, y)와 방향(dirNum) 설정
+        if (k <= n) {  // Top row, facing down
+            x = 0;
+            y = k - 1;
+            dirNum = 2;  // 방향: 하 (남)
+        } else if (k <= 2 * n) {  // Right column, facing left
+            x = k - n - 1;
+            y = n - 1;
+            dirNum = 3;  // 방향: 좌 (서)
+        } else if (k <= 3 * n) {  // Bottom row, facing up
+            x = n - 1;
+            y = 3 * n - k;
+            dirNum = 0;  // 방향: 상 (북)
+        } else {  // Left column, facing right
+            x = 4 * n - k;
+            y = 0;
+            dirNum = 1;  // 방향: 우 (동)
+        }
 
         //배열을 벗어날 때까지 이동시키고 count
         int cnt = 1;
