9338f34df9cd011f552451cee59bade4430e7379
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-26 20:18:49 +0900

    Title: 거울에 레이저 쏘기 2, Time: 246ms, Memory: 15MB, Status: Passed - CodeTree

--- 240826/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240826/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -1,85 +1,109 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static boolean inRange(int x, int y, int n){
-        if (x < 0 || x >= n || y < 0|| y >= n)
-            return false;
-        return true;
+    static int dir;
+    // /를 만날 때 방향 변화
+    static int decision1(int dir) {
+        if(dir == 0) {
+            dir = 1;
+        }
+        else if(dir == 1){
+            dir = 0;
+        }
+        else if(dir == 2){
+            dir = 3;
+        }
+        else 
+        dir = 2;
+
+        return dir;
     }
+    // \를 만날 때 방향 변화
+    static int decision2(int dir) {
+        if(dir == 0){
+            dir = 3;
+        }
+        else if(dir == 1){
+            dir = 2;
+        }
+        else if(dir == 2){
+            dir = 1;
+        }
+        else 
+        dir = 0;
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        //상, 우, 하, 좌
-        int [] dx = {-1,  0,  1,  0};
-        int [] dy = { 0,  1,  0, -1};
+        return dir;
+    }
+    //마지막으로 튕겨 나오는지 확인
+    static boolean Check(int i, int j, int n) {
+        return 0 <= i && i < n && 0 <= j && j < n;
+    }
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
 
-        //n 입력, n 크기의 배열 생성
-        int n = input.nextInt();
-        char [][] arr = new char[n][n];
+        char[][] arr = new char[n][n];
 
-        //맵의 정보
-        for (int i = 0; i < n; i++){
-            String s = input.next();
-
-            for (int j = 0; j < s.length(); j++){
-                arr[i][j] = s.charAt(j);
+        for(int i = 0; i < n; i ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            for(int j = 0; j < n; j++) {
+                char b;
+                if(str.charAt(j) == '/')
+                b = '/';
+                else
+                b = '\\';
+                arr[i][j] = b;
             }
         }
 
-        //k 입력
-        int k = input.nextInt();
-
-        int x = 0;
-        int y = 0;
-
-        int dirNum = 0;
-
-        // 시작 위치 k에 따라 초기 좌표(x, y)와 방향(dirNum) 설정
-        if (k <= n) {  // Top row, facing down
-            x = 0;
-            y = k - 1;
-            dirNum = 2;  // 방향: 하 (남)
-        } else if (k <= 2 * n) {  // Right column, facing left
-            x = k - n - 1;
-            y = n - 1;
-            dirNum = 3;  // 방향: 좌 (서)
-        } else if (k <= 3 * n) {  // Bottom row, facing up
-            x = n - 1;
-            y = 3 * n - k;
-            dirNum = 0;  // 방향: 상 (북)
-        } else {  // Left column, facing right
-            x = 4 * n - k;
-            y = 0;
-            dirNum = 1;  // 방향: 우 (동)
+        int k = Integer.parseInt(br.readLine());
+        int i;
+        int j;
+        //k의 범위에 따른 레이저 들어오는 방향 설정 및 시작 위치 설정
+        if(1 <= k && k <= n){
+            dir = 0;
+            i = 0;
+            j = k -1;
         }
-
-        //배열을 벗어날 때까지 이동시키고 count
-        int cnt = 1;
-
-        while (true) {
-            cnt++;
-            //상우하좌
-            if (arr[x][y] == '/'){
-                if (dirNum == 0 || dirNum == 2)
-                    dirNum = (dirNum + 3) % 4;
-                else if (dirNum == 1 || dirNum == 3)
-                    dirNum = (dirNum + 1) % 4;
-            }
-            else if (arr[x][y] == '\\'){
-                if (dirNum == 1 || dirNum == 3)
-                    dirNum = (dirNum + 3) % 4;
-                else if (dirNum == 0 || dirNum == 2)
-                    dirNum = (dirNum + 1) % 4;
-            }
-
-            x += dx[dirNum]; y += dy[dirNum];
-
-            if (!inRange(x, y, n)) break;
+        else if(n + 1 <= k && k <= 2 * n){
+            dir = 1;
+            i = k - n - 1;
+            j = n -1;
         }
+        else if(2 * n + 1 <= k && k <= 3 * n){
+            dir = 2;
+            i = n - 1;
+            j = 3 * n - k;
+        }
+        else {
+            dir = 3;
+            i = 4 * n - k;
+            j = 0;
+        }
+        //dx dy 설정
+        int[] dx = new int[] {1, 0, -1, 0};
+        int[] dy = new int[] {0, -1, 0, 1};
+        int cnt = 0;
 
-        System.out.println(cnt);
+        while(true) {
+            if(arr[i][j] == '/') {
+                dir = decision1(dir);
+                cnt++;
+            }
+            else {
+                dir = decision2(dir);
+                cnt++;
+            }
+            i += dx[dir];
+            j += dy[dir];
+
+            boolean check = Check(i, j, n);
+
+            if(!check) break;
+        }
+        System.out.print(cnt);
     }
 }
-
-//진행 방향 + 거울의 방향에 따른 경우의 수
-//4 * 2 = 8개
