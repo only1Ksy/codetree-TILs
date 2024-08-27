4cf787545cfa910cfb4a07f9309d3888fafb00cc
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-27 18:30:52 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 218ms, Memory: 12MB, Status: Passed - CodeTree

--- 240827/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 240827/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -7,20 +7,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //오, 위, 왼, 아 (하나 증가할 때마다 반시계방향으로 회전)
-        int[] dx = {0,-1,  0,  1};
-        int[] dy = {1, 0, -1,  0};
+        //왼 위 오 아 (하나 증가할 때마다 반시계방향으로 회전)
+        int[] dx = { 0, -1,  0,  1};
+        int[] dy = {-1,  0,  1,  0};
 
         int n = input.nextInt();
 
         int [][] arr = new int [n][n];
 
-        int x = n/2, y = n/2;
+        int x = n-1, y = n-1;
         int dirNum = 0;
-        arr[x][y] = 1;
+        arr[x][y] = n*n;
 
         //두 번째 칸 (0, 1)에 2부터 시작해서 넣어야 하므로 범위 설정
-        for (int i = 2; i <= n*n; i++){
+        for (int i = n*n - 1; i > 0; i--){
             int nx = x + dx[dirNum], ny = y + dy[dirNum];
 
             if(!inRange(nx, ny, n) || arr[nx][ny] != 0)
@@ -28,7 +28,7 @@ public class Main {
 
             //방향을 바꾼 다음 이동해야 하므로, x = nx가 아닌 새로운 dirNum 이용한 값 대입
             x += dx[dirNum]; y += dy[dirNum];
-            
+
             arr[x][y] = i;
         }
 
