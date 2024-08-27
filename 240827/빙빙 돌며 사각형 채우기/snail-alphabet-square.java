c0b42783cfa1c919442568e8d15804e30794dbef
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-27 17:59:01 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 225ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240827/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static boolean inRange(int x, int y, int n, int m) {
+        return (0 <= x && x < n && 0 <= y && y < m);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //오, 아, 왼, 위 (하나 증가할 때마다 시계방향으로 회전)
+        int[] dx = {0, 1,  0, -1};
+        int[] dy = {1, 0, -1,  0};
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        char [][] arr = new char [n][m];
+
+        int x = 0, y = 0;
+        int dirNum = 0;
+        arr[x][y] = 'A';
+
+        //두 번째 칸 (0, 1)에 2부터 시작해서 넣어야 하므로 범위 설정
+        for (int i = 2; i <= n*m; i++){
+            int nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+            if(!inRange(nx, ny, n, m) || arr[nx][ny] != 0)
+                dirNum = (dirNum + 1) % 4;
+
+            //방향을 바꾼 다음 이동해야 하므로, x = nx가 아닌 새로운 dirNum 이용한 값 대입
+            x = x + dx[dirNum]; y = y + dy[dirNum];
+            arr[x][y] = (char)('A' + ((i - 1)%'A'));
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
