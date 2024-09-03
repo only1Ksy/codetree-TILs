7bc3abde5b9d4ca45d02c66dc106cb0917039a35
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-03 20:22:06 +0900

    Title: 숨은 단어 찾기 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/숨은 단어 찾기 2/find-hidden-words-2.java
+++ 240903/숨은 단어 찾기 2/find-hidden-words-2.java
@@ -30,12 +30,12 @@ public class Main {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 'L') {
                     for (int k = 0; k < 4; k++) {
-                        int num = 0;
                         int x = i;
                         int y = j;
+                        int num = 0;
 
-                        //기준점으로부터 LEE 찾기
-                        while (true) {
+                        // 기준점으로부터 LEE 찾기
+                        for (int step = 1; step <= 2; step++) {
                             x += dx[k];
                             y += dy[k];
 
@@ -50,24 +50,17 @@ public class Main {
                             result++;
                         }
                     }
-                }
-                else if (arr[i][j] == 'E') {
+                } else if (arr[i][j] == 'E') {
                     for (int k = 0; k < 4; k++) {
-                        int num = 0;
-                        int x = i;
-                        int y = j;
-                        int x1 = i;
-                        int y1 = j;
+                        int x1 = i + dx[k];
+                        int y1 = j + dy[k];
+                        int x2 = i + 2 * dx[k];
+                        int y2 = j + 2 * dy[k];
 
-                        //기준점으로부터 EEL 찾기
-                        x += dx[k]; x1 += 2*dx[k];
-                        y += dy[k]; y1 += 2*dy[k];
-
-                        if (inRange(x, y, n, m) && arr[x][y] == 'E' && arr[x1][y1] == 'L') {
+                        // 기준점으로부터 EEL 찾기
+                        if (inRange(x1, y1, n, m) && inRange(x2, y2, n, m) 
+                            && arr[x1][y1] == 'E' && arr[x2][y2] == 'L') {
                             result++;
-                            break;
-                        } else {
-                            break;
                         }
                     }
                 }
