1642a9ac3df5001713e18f544f3d09e71ed327df
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-04 11:19:42 +0900

    Title: 최고의 13위치 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240904/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int [][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                arr[i][j] = input.nextInt();
+            }
+        }
+
+        int num = 0;
+
+        //첫번째 격자
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n-2; j++){
+                //두번째 격자
+                for (int k = i+1; k < n; k++){
+                    for (int l = 0; l < n-2; l++){
+                        int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2];
+
+                        num = Math.max(num, sum);
+                    }
+                }
+            }
+        }
+
+        System.out.println(num);
+    }
+}
