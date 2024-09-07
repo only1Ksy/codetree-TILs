9d3d20bd26aa7b9d2513cd09e029b8c8770098a9
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-07 19:52:50 +0900

    Title: 바구니 안의 사탕 2, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240907/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240907/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -7,23 +7,24 @@ public class Main {
         int n = input.nextInt();
         int k = input.nextInt();
 
-        int [] num = new int[n];
-        int [] pla = new int[n];
-        int [] arr = new int[100 + 1];
+        int[] num = new int[n];
+        int[] pla = new int[n];
+        int[] arr = new int[100 + 1];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             num[i] = input.nextInt();
             pla[i] = input.nextInt();
-
             arr[pla[i]] += num[i];
         }
 
         int max = 0;
-    
-        for (int i = 0 + k; i <= 100 - k; i++){
+
+        // 중심점 c를 0부터 100까지 반복
+        for (int i = 0; i <= 100; i++) {
             int sum = 0;
 
-            for (int j = i-k; j <= i+k; j++){
+            // [i-k, i+k] 구간의 사탕 수 계산
+            for (int j = Math.max(0, i - k); j <= Math.min(100, i + k); j++) {
                 sum += arr[j];
             }
 
