122313dd9d3612f5bdb006f1060e40e1fe5c2f2a
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-18 18:02:31 +0900

    Title: 스승의 은혜 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240918/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] p = new int[n];
+        int b = input.nextInt();
+
+        for (int i = 0; i < n; i++){
+            p[i] = input.nextInt();
+        }
+
+        int max = 0;
+
+        // 할인을 받을 학생을 선택
+        for (int i = 0; i < n; i++){
+            int sum = 0;
+            int pp = 0;
+
+            for (int j = 0; j < n; j++){
+                if (i == j){
+                    sum += p[j]/2; pp++;
+                    continue;
+                }
+                sum += p[j];
+
+                if (sum > b){
+                    sum -= p[i];
+                }
+                else pp++;
+            }
+
+            max = Math.max(max, pp);
+        }
+
+        System.out.println(max);
+    }
+}
