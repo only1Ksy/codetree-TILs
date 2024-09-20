fd10b2d4614c416af7e6d99e6d9c9cddead3bf0d
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-20 13:11:38 +0900

    Title: 이상한 폭탄 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240920/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        int [] a = new int[n];
+
+        for (int i = 0; i < n; i++){
+            a[i] = input.nextInt();
+        }
+
+        int max = -1;
+
+        for (int i = 0; i < n-k; i++){
+            ArrayList <Integer> arr = new ArrayList<>();
+
+            for (int j = i; j <= i + k; j++){
+                if (arr.contains(a[j])){
+                    max = Math.max(max, a[j]);
+                }
+                arr.add(a[j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
