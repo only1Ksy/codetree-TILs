52c4d13faf2e677dbda2c8cc723d03fef8cea495
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-26 21:25:44 +0900

    Title: 상해버린 치즈, Time: 199ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240926/상해버린 치즈/rotten-cheese.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Person {
+    int personNum;
+    int eatenCh;
+    int eatenT;
+    int sickT;
+
+    public Person(){}
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        Person[] p = new Person[n+1];
+        for (int i = 0; i < n+1; i++){
+            p[i] = new Person();
+        }
+
+        int M = input.nextInt();
+        // 상한 치즈 1, 멀쩡한 치즈 0
+        boolean[] m = new boolean[M+1];
+
+        int d = input.nextInt();
+        int s = input.nextInt();
+
+        // 몇 번째 사람이, 몇 번째 치즈를, 몇 초에 먹었는가?
+        Person[] p1 = new Person[d+1];
+        for (int i = 1; i <= d; i++){
+            p1[i] = new Person();
+
+            p1[i].personNum = input.nextInt();
+            p1[i].eatenCh = input.nextInt();
+            p1[i].eatenT = input.nextInt();
+        }
+
+        // 몇 번째 사람이, 몇 초에 아팠는가?
+        for (int i = 0; i < s; i++){
+            int pNum = input.nextInt();
+            p[pNum].sickT = input.nextInt();
+        }
+
+        // 상한 치즈
+        // 1-m 치즈까지 상한 치즈 감별 (아픈 사람이 아파지기 전에 먹었는가?)
+        for (int i = 1; i <= n; i++){
+            if (p[i].sickT != 0){ // 아픈 사람 번호 i
+                for (int j = 1; j <= d; j++){
+                    if (p1[j].personNum == i && p1[j].eatenT < p[i].sickT){
+                        m[p1[j].eatenCh] = true; // 먹은 치즈가 상함
+                    }
+                }
+            }
+        }
+
+        // 상한 치즈 먹은 사람 수 세기 (중복 방지 설정)
+        // set은 자동으로 중복 입력을 방지해 줌
+        int max = 0;
+
+        for (int i = 1; i <= M; i++){
+            if (m[i]){ // i 번째 치즈가 상했다고 가정
+                Set<Integer> medicatedPeople = new HashSet<>();
+
+                for (int j = 1; j <= d; j++){
+                    if (p1[j].eatenCh == i) 
+                        medicatedPeople.add(p1[j].personNum);
+                }
+
+                max = Math.max(max, medicatedPeople.size());
+            }
+        }
+
+        System.out.println(max);
+    }
+}
