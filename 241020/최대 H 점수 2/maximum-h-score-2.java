8dd48da4afb27fb57355fc250f5b3ee34c1276ac
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-10-20 18:19:03 +0900

    Title: 최대 H 점수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241020/최대 H 점수 2/maximum-h-score-2.java
+++ 241020/최대 H 점수 2/maximum-h-score-2.java
@@ -22,11 +22,21 @@ public class Main {
         // h값을 기준으로 반복문 설정
         for (int i = max; i > -1; i--){
             // h값에 가깝지만 작은 배열 요소에 1씩 더해주기
-            for (int j = Math.max(0, Math.min(i, n)) - l; j < l; j++){
-                arr[j] += 1;
+            // 1 2 3 100, i == 3, l == 1
+            int cnt = 0;
+            int startIdx = 0;
+            for (int j = n-1; j > -1; j--){
+                if (arr[j] < i) {
+                    if (cnt == 0) {
+                        startIdx = j;
+                    }
+                    arr[j] += 1;
+                    cnt++;
+                }
+                if (cnt == l) break;
             }
 
-            int cnt = 0; // h 이상인 수의 개수
+            cnt = 0; // h 이상인 수의 개수
 
             for (int j = 0; j < n; j++){
                 if (arr[j] >= i) cnt++;
@@ -36,7 +46,14 @@ public class Main {
                 result = i;
                 break;
             }
+
+            // 배열 원상복귀
+            for (int j = startIdx; j > startIdx-l; j--){
+                arr[j] -= 1;
+            }
         }
+
+        
 
         System.out.println(result);
     }
