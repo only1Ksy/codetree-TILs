1219bb123bc055d72e24f0ace074ac8e90b3d89a
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-10-25 15:11:13 +0900

    Title: 전부 겹치는 선분 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241025/전부 겹치는 선분 2/overlapping-line-segments-2.java
+++ 241025/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -8,7 +8,7 @@ public class Main {
         int[] x1 = new int[n];
         int[] x2 = new int[n];
         int maxX1 = Integer.MIN_VALUE;
-        int maxX2 = Integer.MAX_VALUE;
+        int maxX2 = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++){
             x1[i] = input.nextInt();
@@ -19,12 +19,15 @@ public class Main {
         }
 
         int max2x1 = Integer.MIN_VALUE;
+        int max2x2 = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++){
-            if (x1[i] == maxX1) continue;
-            max2x1 = Math.max(max2x1, x1[i]);
+            if (x1[i] != maxX1) 
+                max2x1 = Math.max(max2x1, x1[i]);
+            if (x2[i] != maxX2)
+                max2x2 = Math.max(max2x2, x2[i]);
         }
 
-        if (max2x1 > maxX2) 
+        if (max2x1 > maxX2 && maxX1 > max2x2) 
             System.out.println("No");
         else 
             System.out.println("Yes");
