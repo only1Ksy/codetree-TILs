5a12b0ccc0a6084bd2e881e30d9fe48071fa9fc4
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-10-25 15:13:53 +0900

    Title: 전부 겹치는 선분 2, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/전부 겹치는 선분 2/overlapping-line-segments-2.java
+++ 241025/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -8,26 +8,26 @@ public class Main {
         int[] x1 = new int[n];
         int[] x2 = new int[n];
         int maxX1 = Integer.MIN_VALUE;
-        int maxX2 = Integer.MIN_VALUE;
+        int minX2 = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++){
             x1[i] = input.nextInt();
             x2[i] = input.nextInt();
 
             maxX1 = Math.max(maxX1, x1[i]);
-            maxX2 = Math.max(maxX2, x2[i]);
+            minX2 = Math.min(minX2, x2[i]);
         }
 
         int max2x1 = Integer.MIN_VALUE;
-        int max2x2 = Integer.MIN_VALUE;
+        int min2x2 = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++){
             if (x1[i] != maxX1) 
                 max2x1 = Math.max(max2x1, x1[i]);
-            if (x2[i] != maxX2)
-                max2x2 = Math.max(max2x2, x2[i]);
+            if (x2[i] != minX2)
+                min2x2 = Math.min(min2x2, x2[i]);
         }
 
-        if (max2x1 > maxX2 && maxX1 > max2x2) 
+        if (max2x1 > minX2 && maxX1 > min2x2) 
             System.out.println("No");
         else 
             System.out.println("Yes");
