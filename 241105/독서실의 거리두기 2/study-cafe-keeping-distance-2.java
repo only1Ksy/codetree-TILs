fa361749b8a055f6a1df353db6ad417ba2090dd6
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-11-05 11:47:39 +0900

    Title: 독서실의 거리두기 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241105/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 241105/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -8,6 +8,7 @@ public class Main {
         char[] c = input.next().toCharArray();
 
         int preIdx = 0; int postIdx = 0;
+        int first1 = 0, last1 = 0;
         int tempIdx = -1; int maxDis = Integer.MIN_VALUE;
         int minDis = Integer.MAX_VALUE;
 
@@ -15,9 +16,11 @@ public class Main {
         for (int i = 0; i < n; i++){
             if (c[i] == '1' && tempIdx == -1){
                 tempIdx = i;
+                first1 = i;
             }
             else if (c[i] == '1'){
                 int distance = i - tempIdx;
+                last1 = i;
 
                 if (distance > maxDis){
                     preIdx = tempIdx; postIdx = i;
@@ -32,6 +35,14 @@ public class Main {
 
         int subPre = 0, subPost = 0;
 
+        if (first1 == 0) first1 = Integer.MIN_VALUE;
+        else first1 = first1 - 0;
+        if (last1 == n) last1 = Integer.MIN_VALUE;
+        else last1 = n-1 - last1;
+
+        // first / last 중 하나에 넣었을 때 값
+        int tempVal = Math.max(first1, last1);
+
         if ((preIdx + postIdx)%2 == 0){
             int i = (preIdx + postIdx) / 2;
             c[i] = 1;
@@ -41,8 +52,9 @@ public class Main {
             c[i] = 1;
             subPre = i - preIdx; subPost = postIdx - i;
         }
+        int tempVal2 = Math.min(subPost, subPre);
 
-        minDis = Math.min(minDis, Math.min(subPre, subPost));
+        minDis = Math.min(minDis, Math.max(tempVal, tempVal2));
 
         System.out.println(minDis);
     }
