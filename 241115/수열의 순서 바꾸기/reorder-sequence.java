43c8f86d977659b48b212599a53afdd3232957b2
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-11-15 21:34:30 +0900

    Title: 수열의 순서 바꾸기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241115/수열의 순서 바꾸기/reorder-sequence.java
+++ 241115/수열의 순서 바꾸기/reorder-sequence.java
@@ -13,23 +13,20 @@ public class Main {
             arr[i] = input.nextInt();
         }
 
-        int maxLength = 1;  // 가장 긴 오름차순 부분 수열의 길이
-        int tempLength = 1; // 현재 오름차순 부분 수열의 길이
-
-        // 가장 긴 오름차순 부분 수열을 찾기
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] < arr[i + 1]) {  // 오름차순일 때
-                tempLength++;  // 길이를 증가시킴
-            } else {  // 오름차순이 끊어졌을 때
-                maxLength = Math.max(maxLength, tempLength);  // 가장 긴 오름차순 길이 갱신
-                tempLength = 1;  // 새로운 오름차순 부분 수열 시작
+        int cnt = 0;
+        
+        for(int i = 0; i < n; i++) {
+            boolean flag = false;
+            for(int j = i; j < n - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    flag = true;
+                }
             }
+            if(flag) cnt++;
         }
 
-        // 마지막 부분까지 확인해서 길이 갱신
-        maxLength = Math.max(maxLength, tempLength);
 
-        // 최소 횟수는 전체 길이에서 가장 긴 오름차순 수열 길이를 뺀 값
-        System.out.println(n - maxLength);
+        // 최소 횟수는 전체 길이에서 가장 긴 오름차순 부분 수열 길이를 빼면 됨
+        System.out.println(n == 1 ? 0 : cnt);
     }
 }
