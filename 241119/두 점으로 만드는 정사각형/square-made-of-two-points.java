52c9620c6625300bbe70802e93929c172844813d
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-11-19 12:20:09 +0900

    Title: 두 점으로 만드는 정사각형, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241119/두 점으로 만드는 정사각형/square-made-of-two-points.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x1 = input.nextInt();
+        int y1 = input.nextInt();
+        int x2 = input.nextInt();
+        int y2 = input.nextInt();
+
+        int a1 = input.nextInt();
+        int b1 = input.nextInt();
+        int a2 = input.nextInt();
+        int b2 = input.nextInt();
+
+        int result = Math.max((Math.max(y2, b2) - Math.min(y1, b1)),
+                     (Math.max(x2, a2) - Math.min(x1, a1)));
+    
+        result *= result;
+
+        System.out.println(result);
+    }
+}
